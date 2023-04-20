6d3b50675ae1c633dce88a57feff06465ed61e22
apollo5ever <backend@ambrosia.money>
2023-04-20 16:55:18 -0600

    added try/catch to getSC

--- functions/getSC.js
+++ functions/getSC.js
@@ -11,30 +11,37 @@ const data = JSON.stringify({
     }
   });
 
-async function getSC(req,res,next){
+  async function getSC(req, res, next) {
     console.log("getting sc")
-    fetch(`https://dero-api.mysrv.cloud/json_rpc`, {
+    let tries = 0
+  
+    try {
+      const response = await fetch(`https://dero-api.mysrv.cloud/json_rpc`, {
         method: 'POST',
         body: data,
         headers: {'Content-Type': 'application/json' }
-      }).then(res => res.json())
-      .then((json) => {   
-            
-            var scData = json.result.stringkeys 
-
-            var islandSearch= /.*_M\b/
+      })
   
-          
-          
-          req.names= Object.keys(scData)
-          .filter(key => islandSearch.test(key))
-          .map(key=>key.substring(0,key.length-2))
-         
-            
-            req.scData=scData
-            next()
+      const json = await response.json()
+      const scData = json.result.stringkeys 
+      const islandSearch = /.*_M\b/
+      req.names = Object.keys(scData)
+        .filter(key => islandSearch.test(key))
+        .map(key => key.substring(0, key.length - 2))
+      req.scData = scData
+      next()
+  
+    } catch (err) {
+      tries++
+      console.error(`Error fetching SC data, try ${tries}:`, err)
+      if (tries < 3) {
+        setTimeout(getSC, 1000, req, res, next)
+      } else {
+        console.error('Max retries reached. Giving up.')
+        next()
       }
-      )
-}
+    }
+  }
+  
 
 module.exports = getSC
